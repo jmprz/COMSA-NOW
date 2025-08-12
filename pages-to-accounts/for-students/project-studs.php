7b61f5b477a45b147067daacae03becc5cdb82a2
Valexore <jayaruntalanb22@gmail.com>
2025-08-12 07:28:41 -0700

    modified:   assets/css/project-studs-design.css 	modified:   pages-to-accounts/for-students/project-studs.php 	modified:   pages-to-accounts/for-students/student-dashboard.php

--- pages-to-accounts/for-students/project-studs.php
+++ pages-to-accounts/for-students/project-studs.php
@@ -16,6 +16,10 @@ require_once '../../../backend/middleware/student_middleware.php';
   <meta name="keywords" content="student projects, project showcase, game development, web development">
 
   <!-- Favicons -->
+
+
+
+  
   <link rel="apple-touch-icon" sizes="180x180" href="../../assets/img/favicon/apple-touch-icon.png">
   <link rel="icon" type="image/png" sizes="32x32" href="../../assets/img/favicon/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="../../assets/img/favicon/favicon-16x16.png">
